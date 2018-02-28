524a0108cfb3b08950548177a7258258d5acfbe1
Grissom <grissom.wang@daocloud.io>
2018-02-28 23:00:53 +0800

    update

--- src/main/java/io/daocloud/labs/docker/config/ConfigServerApplication.java
+++ src/main/java/io/daocloud/labs/docker/config/ConfigServerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+
 @SpringBootApplication
 @EnableConfigServer
+@EnableSwagger2Doc
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
