669aa15c8fa99e1650fe9c5b0fdb1b792be87611
Grissom <grissom.wang@daocloud.io>
2018-03-24 07:51:57 +0800

    enable discoverclient

--- src/main/java/daocloud/nat/labs/dmp/config/ConfigServerApplication.java
+++ src/main/java/daocloud/nat/labs/dmp/config/ConfigServerApplication.java
@@ -1,13 +1,15 @@
-package io.daocloud.labs.docker.config;
+package daocloud.nat.labs.dmp.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 @EnableSwagger2Doc
 public class ConfigServerApplication {
 
