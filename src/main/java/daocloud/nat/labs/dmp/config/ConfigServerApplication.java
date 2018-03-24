f7b2b17cdc1e2c756f0564eac9375489b390084b
Grissom <grissom.wang@daocloud.io>
2018-03-24 09:15:24 +0800

    refactor

--- src/main/java/daocloud/nat/labs/dmp/config/ConfigServerApplication.java
+++ src/main/java/daocloud/nat/labs/dmp/config/ConfigServerApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
-
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
